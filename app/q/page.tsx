82ec6cf57cb867fcd481b0d2e634297ccb7edd7a
mike-b-m <mikebarrymichel@gmail.com>
2025-04-29 14:05:45 -0400

    correct

--- app/q/page.tsx
+++ app/q/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         
         <label>
         <input type='text' name="search" placeholder="Mete mo a!" className="rounded-l-2xl placeholder:italic placeholder:text-center focus:outline-none focus:pl-4" onChange={(e) =>setNam(e.target.value)}/>
-        <button type="submit" className=" rounded-r-2xl place-items-center pt-1 hover:text-blue-500" onClick={()=>setName(nam)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-6">
+        <button type="submit" className=" rounded-r-2xl place-items-center pt-1 hover:text-blue-500" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 </svg></button>
         </label>
