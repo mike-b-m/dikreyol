29c30fbdb307d990526edff57a1bfcc0cedd1a2b
mike-b-m <mikebarrymichel@gmail.com>
2025-04-29 22:54:04 -0400

    correct

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page(){
             <h1 className="lg:text-5xl text-bold text-3xl">Diksyone Kreyol</h1>
             <form method="get" action="/q" className=" xl:m-4 shadow-md bg-sky-200  xl:pl-3 rounded-2xl">
             <label className="input">
-            <input name="search" placeholder="Mete mo a!" className="rounded-l-2xl lg:w-80 w-60 placeholder:italic placeholder:text-center focus:outline-none focus:pl-4"/>
+            <input name="search" placeholder="Mete mo a!" className="rounded-l-2xl w-60 placeholder:italic placeholder:text-center focus:outline-none focus:pl-4"/>
             <button type="submit" className=" rounded-r-2xl place-items-center pt-1 hover:text-blue-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 </svg>
