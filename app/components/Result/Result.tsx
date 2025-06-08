807aff1dcd805c2d98f5f876487d33ab2a736eb8
mike-b-m <mikebarrymichel@gmail.com>
2025-06-08 14:15:37 -0400

    button next&prev

--- app/components/Result/Result.tsx
+++ app/components/Result/Result.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Suspense, useEffect, useState} from "react";
+import {Suspense, useEffect, useRef, useState} from "react";
 import {supabase} from "../db";
 import { useSearchParams } from "next/navigation";
 import Search from "../SearchBar/Search";
@@ -18,7 +18,10 @@ type user = {
 function Result() {
   const [word, setWord] = useState<user[]>([])
   const [loding, setLoding] = useState(true)
-    
+  const [num, setNum] = useState(0)
+  const [role, setRole] = useState(20)
+  const [comb, setComb] = useState(1)
+  
     const searchpara = useSearchParams();
     const search = searchpara.get('search') || '';
     
@@ -34,6 +37,19 @@ function Result() {
     
     const n = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase()
   const fil = word.filter(wd =>wd.word === n)
+  {/*bouton next ak prev mo dispo*/}
+  const next = ()=> {
+    if (role < word.length) {
+    setNum((comb)*20)
+    setRole((comb+1)*20)
+    setComb(comb+1)
+  }}
+  const prev= ()=>{
+    if (num>0) {
+    setNum((comb-2)*20)
+    setRole((comb-1)*20)
+    setComb(comb-1)
+  }}
           
   return (
     <div className="min-h-screen flex flex-col">
@@ -71,7 +87,7 @@ function Result() {
                     <h1 className="bg-clo h-1 mt-2 mb-2">_</h1>
                     <div><span className="font-bold">Sinonim:</span> {user.sino}</div>
                     <h1 className="bg-clo h-1 mt-2 mb-2">_</h1>
-                    <div><span className="font-bold">Kont: </span>{user.kont}</div>
+                    <div><span className="font-bold">Antonim: </span>{user.kont}</div>
                   </li>
                 ))}
               </ul>
@@ -81,10 +97,10 @@ function Result() {
           </div>
         </div>
         {/* Mo disponib */}
-        <div className="bg-white rounded-lg shadow p-4">
+        <div className="bg-white rounded-lg shadow p-4 ">
           <h2 className="font-bold text-xl mb-3 bg-clo pl-2">Mo disponib:</h2>
-          <ul className="columns-2 md:columns-3 lg:columns-4">
-            {word.map((user) => (
+          <ul className="columns-2 h-60 md:columns-3 lg:columns-4 md:h-35">
+            {word.slice(num,role).map((user) => (
               <li key={user.id}>
                 <a href={`/results?search=${user.word}`} className="hover:text-blue-500 cursor-pointer">
                   {user.word}
@@ -92,6 +108,17 @@ function Result() {
               </li>
             ))}
           </ul>
+            {/*button next*/}
+          <div className="text-center italic ">
+          {comb-1 > 0 ?<button className="mr-2 hover:text-[#6FE6FC]" onClick={prev}>avan</button>:null}
+          {comb-1 > 0 ?(<button className="mr-2 h-6 w-6 rounded-lg border-1 border-sky-200">{comb-1}</button>): null}
+          
+          <button className="mr-2 h-6 w-6 rounded-lg font-bold bg-clo">{comb} </button>
+          {role < word.length ?<button className="mr-2 h-6 w-6 rounded-lg border-1 border-sky-200 "> {comb+1} </button>:null}
+          {role < word.length ?<button onClick={next} className="hover:text-[#6FE6FC]">
+           suivan
+          </button>: null}
+           </div>
         </div>
       </div>
     </div>
